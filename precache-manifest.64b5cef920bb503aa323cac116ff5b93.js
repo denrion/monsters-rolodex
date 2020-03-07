8e41dfc56e0ce542076553d9774f56205f6a7adb
Nemanja Kuzmanovic <kuzmaneca@gmail.com>
2020-03-07 16:11:30 +0100

    Updates

---
+++ precache-manifest.64b5cef920bb503aa323cac116ff5b93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "643a9115443d26cc48d35a20d5de4c84",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "225faa7fbea1221608cc",
+    "url": "/monsters-rolodex/static/css/main.ebdb30db.chunk.css"
+  },
+  {
+    "revision": "a683d3c15696d61584b7",
+    "url": "/monsters-rolodex/static/js/2.a234ae2c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.a234ae2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225faa7fbea1221608cc",
+    "url": "/monsters-rolodex/static/js/main.a5b557f5.chunk.js"
+  },
+  {
+    "revision": "ed75eac08a3bf1a4b090",
+    "url": "/monsters-rolodex/static/js/runtime-main.1d84e0ed.js"
+  }
+]);
